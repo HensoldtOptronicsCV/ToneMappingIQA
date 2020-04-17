5071d9f22f70c097ba727834353d45ec71f08dad
Teutsch, Michael <michael.teutsch@hensoldt.net>
2020-04-17 18:01:21 +0200

    MIT license text added and some minor fixes in the code

--- evaluation/exposure_measure/exposure_measure.py
+++ evaluation/exposure_measure/exposure_measure.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2020 HENSOLDT
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 # *
 # Python script to calculate the exposure measure provided by Eilertsen et al.
 # Link to paper: https://www.cl.cam.ac.uk/~rkm38/pdfs/eilertsen2017tmo_star.pdf
@@ -13,9 +35,9 @@ def calcExposureMeasure(image):
     # calculate histogram of the image
     hist, bins = np.histogram(image, 256, [0, 255])
     # fraction of under-exposed pixels
-    under_exp_pix = sum(hist[0:int(256*0.02)])/sum(hist) * 100
+    under_exp_pix = sum(hist[0:int(255*0.02)])/sum(hist) * 100
     # fraction of over-exposed pixels
-    over_exp_pix = sum(hist[int(256*0.95):])/sum(hist) * 100
+    over_exp_pix = sum(hist[int(255*0.95):])/sum(hist) * 100
     
     return under_exp_pix, over_exp_pix
 
--- plots/plot_lossofcontrast_global_clipping.py
+++ plots/plot_lossofcontrast_global_clipping.py
@@ -1,3 +1,25 @@
+# MIT License
+#
+# Copyright (c) 2020 HENSOLDT
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,7 +37,7 @@ plt.errorbar(x, y_LTIR, e_LTIR, linestyle='solid', marker='o', color='blue')
 plt.xlabel('clipping')
 plt.ylabel('global loss of contrast')
 plt.legend(['FLIR', 'LTIR'])
-plt.title('Verification of global loss of constrast measure')
+plt.title('Verification of global loss of contrast measure')
 
 plt.show()
 
