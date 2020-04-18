8d5d35b1aa260b77aec4eb2bbcb6c1da0c5ca770
Michael Teutsch <62538471+MichaelTeutsch@users.noreply.github.com>
2020-04-18 13:14:08 +0200

    MIT license text added and some minor fixes in the code (#1)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ImageQualityAssessment
-To be done!
+# Tone Mapping Image Quality Assessment
+This is the code provided with the paper "An Evaluation of Objective Image Quality Assessment for Thermal Infrared Video Tone Mapping" by Teutsch et al., IEEE CVPR Workshops 2020.
+
+If you use the code or the plots or the findings of our paper, then please cite us:
+
+@InProceedings{TeutschCVPRW2020,
+Title = {{An Evaluation of Objective Image Quality Assessment for Thermal Infrared Video Tone Mapping}},
+Author = {Michael Teutsch and Simone Sedelmaier and Sebastian Moosbauer and Gabriel Eilertsen and Thomas Walter},
+Booktitle = {IEEE CVPR Workshops},
+Year = {2020}
+}
--- evaluation/calculateExposureMeasure.py
+++ evaluation/calculateExposureMeasure.py
@@ -1,15 +1,37 @@
+# MIT License
+#
+# Copyright (c) 2020 HENSOLDT
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import sys
-from evaluation.FLIR_data_reader.FLIR_reader import getAllFlirSets
-from evaluation.exposure_measure.exposure_measure import exposure
+from FLIR_Thermal_Dataset_reader.FLIR_Thermal_Dataset_reader import getAllFlirSets
+from exposure_measure.exposure_measure import exposure
 
 dataReader = getAllFlirSets
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        raise IndexError("to few input arguments! Provide absolute Path to FLIR Dataset")
+        raise IndexError("Too few input arguments! Please provide absolute path to 'FLIR Thermal Dataset'.")
 
-    flirPath = sys.argv[1]
-    train, val, video = dataReader(flirPath)
-    under_exposure, over_exposure = exposure(video['8bit'])
-    print("under exposure: ", under_exposure)
-    print("over exposure: ", over_exposure)
+    flirThermalDatasetPath = sys.argv[1]
+    train, val, video = dataReader(flirThermalDatasetPath)
+    underExposure, overExposure = exposure(video['8bit'])
+    print("under-exposure: ", underExposure)
+    print("over-exposure: ", overExposure)
